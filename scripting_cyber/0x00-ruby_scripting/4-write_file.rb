d57033a3755335725c3cf0f1e3c88110a71c9d12
ForgetG <9430@holbertonstudent.com>
2026-05-06 11:36:16 +0200

    task4 v2

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -6,14 +6,9 @@ require 'json'
 # @param file1_path [String] source file
 # @param file2_path [String] destination file
 def merge_json_files(file1_path, file2_path)
-  return unless File.exist?(file1_path)
-  return unless File.exist?(file2_path)
 
   data1 = JSON.parse(File.read(file1_path))
   data2 = JSON.parse(File.read(file2_path))
-
-  return unless data1.is_a?(Array)
-  return unless data2.is_a?(Array)
 
   merged_data = data2 + data1
 
