e7bf9bbe43a1e3eb3b6d90d010dc232d3729893d
ForgetG <9430@holbertonstudent.com>
2026-05-06 11:42:47 +0200

    task7 v@

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,17 +2,12 @@
 
 # Prints command-line arguments
 def print_arguments
-  args = ARGV
-
-  if args.empty?
-    puts 'No arguments provided.'
-    return
-  end
-
-  index = 1
-
-  args.each do |arg|
-    puts "#{index}. #{arg}"
-    index += 1
+  if ARGV.empty?
+    puts "No arguments provided."
+  else
+    puts "Arguments:"
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
+    end
   end
 end
