8aac24e4cbaa73ded88f299775d281f608b07917
ForgetG <9430@holbertonstudent.com>
2026-05-06 11:40:11 +0200

    task4 v4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/env ruby
 require 'json'
 
-# Merges JSON objects from file1 into file2
-# @param file1_path [String] source file
-# @param file2_path [String] destination file
 def merge_json_files(file1_path, file2_path)
-  data1 = JSON.parse(File.read(file1_path))
-  data2 = JSON.parse(File.read(file2_path))
+    file1_data = JSON.parse(File.read(file1_path))
+    file2_data = JSON.parse(File.read(file2_path))
 
-  merged_data = data2 + data1
+    merged_data = file2_data + file1_data
 
-  File.write(file2_path, JSON.pretty_generate(merged_data))
+    File.write(file2_path, JSON.pretty_generate(merged_data))
+    puts "Merged JSON written to #{file2_path}"
 end
